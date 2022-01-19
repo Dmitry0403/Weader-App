c16bcd734a862fc7120beeccf30d390a700b89ab
Dmitry0403 <85955541+Dmitry0403@users.noreply.github.com>
2022-01-19 23:14:12 +0300

    переделал стили на grid

--- src/WeatherInfoTable/WeatherInfoTable.jsx
+++ src/WeatherInfoTable/WeatherInfoTable.jsx
@@ -2,35 +2,36 @@ import css from "./styles.module.css";
 
 export function WeatherInfoTable(props) {
   const { data, value } = props;
-  const titles = ["температура", "ощущается", "давление, гПа", "влажность, %"];
-  const dataItems = ["temp", "feels_like", "pressure", "humidity"];
+  // const titles = ["температура", "ощущается", "давление, гПа", "влажность, %"];
+  // const dataItems = ["temp", "feels_like", "pressure", "humidity"];
+
+  const dataItems = [
+    { title: "температура", dataItem: "temp" },
+    { title: "ощущается", dataItem: "feels_like" },
+    { title: "давление, гПа", dataItem: "pressure" },
+    { title: "влажность, %", dataItem: "humidity" },
+  ];
 
   if (!value) {
     return <div>Введите название города</div>;
   }
   return (
-    <div>
-      <table className={css.infoTable}>
-        <caption>
-          Метеопоказания в городе {value[0].toUpperCase() + value.slice(1)}
-        </caption>
-        <thead>
-          <tr>
-            {titles.map((item) => (
-              <th scope="col" key={item}>
-                {item}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            {dataItems.map((item) => (
-              <td key={item}>{data[item]}</td>
-            ))}
-          </tr>
-        </tbody>
-      </table>
+    <div className={css.infoTable}>
+      <div class={css.caption}>
+        Метеопоказания в городе {value[0].toUpperCase() + value.slice(1)}
+      </div>
+      <div className={css.container}>
+        {dataItems.map((item) => (
+          <div>
+            <div className={css.item} key={item.title}>
+              {item.title}
+            </div>
+            <div className={css.item} key={item.dataItem}>
+              {data[item.dataItem]}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
